00b8b331df80852d994a825cef6e33183e1694d0
EnesKllokoqi1 <eneskllokoqi@gmail.com>
2024-08-20 18:05:41 +0200

    Module 22

---
+++ Module 22/app.js
@@ -0,0 +1,102 @@
+console.log("Good job guys");
+
+var x=5;
+
+var a=50+50;
+
+console.log(a);
+
+var b=10;
+var c=20;
+var d=b+c;
+console.log(d);
+
+var g=c-b;
+console.log(g);
+
+
+var k=10;
+var l=4;
+var m=k%l;
+console.log(m);
+
+
+
+var n=99;
+n++ //postfix
+console.log(n);
+
+var o=101;
+o--
+console.log(o);
+
+var p=5;
+var q=2;
+var h=p**q;
+console.log(h)
+
+var e=15;
+e+=5
+console.log(e)
+
+e-=5
+console.log(e);
+ 
+
+////////////////////////////
+
+
+
+var u=10;
+u*=2
+console.log(u);
+
+u/=2//u=30/2
+console.log(u);
+
+
+
+//////////////////
+
+var t=5;
+var i=5;
+console.log(t==5);
+
+
+var n1=6;
+var n2=7;
+console.log(n1 !=n2)
+
+
+
+console.log(n1>n2)///6>7?  False
+console.log(n1 < n2)//6<7? True
+
+var numri=123;
+var tekst="123"
+console.log(numri == tekst);
+
+var firstname="Digjital"
+var lastName="School"
+var fullname=firstname + " " + lastName;
+console.log(fullName);
+
+
+
+var school="Shkolla Digjitale";
+console.log('Hello from ${school}')
+
+//AND OPERATOR
+var age=24;
+console.log(age>=18 && age <=65);
+
+  //OR OPERATOR
+var num1=5;
+var num2=6
+
+console.log(num1==5 || num2 ==5)
+
+
+var num3=9
+var num4=10
+console.log(!(num3==num4))
